cbd04995e6ab4b668b4363fa79bc47d1ad381262
sshivaji <sshivaji@gmail.com>
2016-12-15 00:17:25 -0800

    page cleanups and build chess_db indexes as well

--- build_game_indexes.sh
+++ build_game_indexes.sh
@@ -2,6 +2,10 @@
 if [ ! -f bases/millionbase.scout ]; then
 	cd bases;scoutfish make millionbase.pgn;cd ..
 fi
+if [ ! -f bases/millionbase.bin ]; then
+        cd bases;parser book millionbase.pgn full;cd ..
+fi
+
 if [ ! -f bases/famous_games.scout ]; then
         cd bases;scoutfish make famous_games.pgn;cd ..
 fi

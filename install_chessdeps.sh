9d217ef432126accb19af794b100931f49465449
Shivkumar Shivaji <sshivaji@gmail.com>
2016-12-17 11:44:54 -0800

    update to latest github versions of chess_db and scoutfish on shells cript

--- install_chessdeps.sh
+++ install_chessdeps.sh
@@ -1,5 +1,13 @@
-git clone https://github.com/mcostalba/scoutfish
+if [ ! -d scoutfish ]; then
+	echo "scoutfish git clone"
+        git clone https://github.com/mcostalba/scoutfish
+fi
+cd scoutfish;git pull; cd ..
 cd scoutfish/src;make build ARCH=x86-64;sudo make install;cd ../..
 
-git clone https://github.com/mcostalba/chess_db
+if [ ! -d chess_db ]; then
+	echo "chess_db git glone"
+        git clone https://github.com/mcostalba/chess_db
+fi
+cd chess_db;git pull; cd ..
 cd chess_db/parser;make build ARCH=x86-64;sudo make install;cd ../..

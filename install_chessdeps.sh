68c0ad3c42999c8a03f6d1b35f49f0c29cabb8ee
Shivkumar Shivaji <sshivaji@gmail.com>
2016-12-17 20:04:51 -0800

    updated libraries and sctripts

--- install_chessdeps.sh
+++ install_chessdeps.sh
@@ -3,11 +3,18 @@ if [ ! -d scoutfish ]; then
         git clone https://github.com/mcostalba/scoutfish
 fi
 cd scoutfish;git pull; cd ..
-cd scoutfish/src;make build ARCH=x86-64;sudo make install;cd ../..
+cd scoutfish/src;make build ARCH=x86-64; cd ../..
+mkdir -p external
+cp scoutfish/src/scoutfish ./external
+cp scoutfish/src/scoutfish.py ./external
+
 
 if [ ! -d chess_db ]; then
 	echo "chess_db git glone"
         git clone https://github.com/mcostalba/chess_db
 fi
 cd chess_db;git pull; cd ..
-cd chess_db/parser;make build ARCH=x86-64;sudo make install;cd ../..
+cd chess_db/parser;make build ARCH=x86-64; cd ../..
+cp chess_db/parser/parser ./external
+cp chess_db/parser/chess_db.py ./external
+

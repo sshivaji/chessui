a4bb35ec2652ef93698a869115e1960c20bde162
sshivaji <sshivaji@gmail.com>
2016-12-19 03:36:03 -0800

    added first time setup script

---
+++ first_time_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./install_chessdeps.sh
+./get_game_data.sh
+./build_game_indexes.sh
+./run.sh
--- get_game_data.sh
+++ get_game_data.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+if [ ! -f bases/millionbase.pgn ]; then
 cd bases; echo 'Getting 2.2M base dataset'; wget https://dl.dropboxusercontent.com/u/19236099/millionbase.pgn.gz; gunzip millionbase.pgn.gz; cd ..
+fi

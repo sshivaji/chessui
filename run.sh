2cb3c972e8d9dc8a1668ea4874ebeb4da736a740
sshivaji <sshivaji@gmail.com>
2017-03-20 21:48:29 +0000

    changed to port 9999

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 virtualenv -ppython2.7 chesspython
 ./chesspython/bin/pip install -r requirements.txt
 #./chesspython/bin/python server.py
-./chesspython/bin/gunicorn -k tornado server:app --bind 0.0.0.0:8888
+./chesspython/bin/gunicorn -k tornado server:app --bind 0.0.0.0:9999
